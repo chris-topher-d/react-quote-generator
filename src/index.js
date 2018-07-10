9a3ef3d48b372bd5eb90ccfa0ed8b501c0effaf6
Christopher Dennis <cdennis.aus@gmail.com>
2018-07-09 22:30:59 -0500

    Did not need registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import QuoteGenerator from './QuoteGenerator';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<QuoteGenerator />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
